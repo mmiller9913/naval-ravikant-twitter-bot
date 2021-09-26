aa37551a14654ab814fdced5b02010e399762cfb
Matt Miller <mmiller9913@gmail.com>
2021-09-26 17:34:21 -0400

    second commit

--- start.js
+++ start.js
@@ -3,7 +3,7 @@ require('dotenv').config({ path: '.env' });
 const app = require('./app');
 app.set('port', process.env.PORT || 3000);
 const server = app.listen(app.get('port'), () => {
-    console.log(`Express running → PORT ${server.address().port}`);
+    console.log(`App started: Express running → PORT ${server.address().port}`);
   });
 
 
